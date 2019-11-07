1ec42aa524d7b12f8c80e4286d4ec9eba9fcbcb1
unknown <anandahlawat90@gmail.com>
2019-11-08 02:07:15 +0530

    deleted duplicate directory

--- beneficiary-service/src/main/java/com/app/revolut/beneficiary/dao/IBeneficiaryDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.revolut.beneficiary.dao;
-
-import com.app.revolut.beneficiary.model.Beneficiary;
-
-public interface IBeneficiaryDao {
-	
-	Beneficiary createBeneficiary(Beneficiary beneficiary) throws Exception;
-
-}
